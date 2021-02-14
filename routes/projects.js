f1ecc7d858df2f732cdeea1a7ebe2e74857c092b
Ismail Aksoy <iaksoy@seanapse.io>
2021-02-14 22:36:00 +0300

    fixed project images and thumbnail urls

--- routes/projects.js
+++ routes/projects.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const { projects } = require("../data.json");
+
+router.get("/", (res, req) => {
+    return res.redirect("/projects/0");
+})
+
+router.get("/:id", (req, res) => {
+    const projectId = req.params.id;
+    const project = projects.find(({id}) => id === +projectId);
+
+    if (project) {
+        res.render("project", {project});
+    } else {
+        res.sendStatus(404);
+    }
+})
+
 module.exports = router;
